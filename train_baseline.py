7eec2c8be9fc85a1ed3abf172818199e64bc2768
Thomas <thomasdemin16@gmail.com>
2022-06-24 17:56:03 +0200

    Polarity classification.

--- train_baseline.py
+++ train_baseline.py
@@ -6,22 +6,57 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import cross_validate, StratifiedKFold
 
-from utils import lol2str
+from utils import lol2str, list2str
 
 
-def main():
-    pol_vectorizer = CountVectorizer()
-    pol_classifier = MultinomialNB()
+def train_subjectivity_classifier():
+    # init classifier and vectorizer for Polairty classification
+    vectorizer = CountVectorizer()
+    classifier = MultinomialNB()
 
+    # get data
+    obj = subjectivity.sents(categories='obj')
+    subj = subjectivity.sents(categories='subj')
+    
+    # build dataset
+    corpus = [list2str(d) for d in obj] + [list2str(d) for d in subj]
+    vectors = vectorizer.fit_transform(corpus)
+    targets = [-1] * len(obj) + [1] * len(subj)
+
+    # train and evaluate
+    scores = cross_validate(classifier, vectors, targets, cv=StratifiedKFold(n_splits=10), scoring=['f1_micro'])
+    scores = np.array(scores['test_f1_micro'])
+
+    return scores
+
+
+def train_polarity_classifier():
+    # init classifier and vectorizer for Polairty classification
+    vectorizer = CountVectorizer()
+    classifier = MultinomialNB()
+
+    # get data
     neg = movie_reviews.paras(categories='neg')
     pos = movie_reviews.paras(categories='pos')
     
-    pol_corpus = [lol2str(d) for d in neg] + [lol2str(d) for d in pos]
-    pol_vectors = pol_vectorizer.fit_transform(pol_corpus)
-    pol_targets = [-1] * len(neg) + [1] * len(pos)
+    # build dataset
+    corpus = [lol2str(d) for d in neg] + [lol2str(d) for d in pos]
+    vectors = vectorizer.fit_transform(corpus)
+    targets = [-1] * len(neg) + [1] * len(pos)
 
-    pol_scores = np.array(cross_validate(pol_classifier, pol_vectors, pol_targets, cv=StratifiedKFold(n_splits=10), scoring=['f1_micro'])['test_f1_micro'])
-    print(f"Naive Bayes F1 score:\n\tAverage: {pol_scores.mean():.2f}\n\tSTD: {pol_scores.std():.2f}")
+    # train and evaluate
+    scores = cross_validate(classifier, vectors, targets, cv=StratifiedKFold(n_splits=10), scoring=['f1_micro'])
+    scores = np.array(scores['test_f1_micro'])
+
+    return scores
+
+
+def main():
+    subj_scores = train_subjectivity_classifier()
+    print(f"Naive Bayes F1 score on subjectivity classification:\n\tAverage: {subj_scores.mean():.2f}\n\tSTD: {subj_scores.std():.2f}")
+
+    pol_scores = train_polarity_classifier()
+    print(f"Naive Bayes F1 score on polarity classification:\n\tAverage: {pol_scores.mean():.2f}\n\tSTD: {pol_scores.std():.2f}")
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -3,3 +3,9 @@ def lol2str(doc):
     List of lists to string.
     """
     return " ".join([w for sent in doc for w in sent])
+
+def list2str(l):
+    """
+    Returns a string from a list
+    """
+    return ' '.join(w for w in l)
