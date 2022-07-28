56bba02b3dc73ccb48cf9f72c5fd515c59808d22
Thomas <thomasdemin16@gmail.com>
2022-07-28 10:49:20 +0200

    Re-organize settings.

--- settings.py
+++ settings.py
@@ -1,23 +1,41 @@
 import os
 import torch
 
+# Paths
 ROOT_DIR=os.path.dirname(__file__)
 SAVE_PATH_BASELINE=os.path.join(ROOT_DIR, 'weights/baseline')
 SAVE_PATH_GRU=os.path.join(ROOT_DIR, 'weights/gru')
 SAVE_PATH_TRANSFORMER=os.path.join(ROOT_DIR, 'weights/transformer')
+
+# CV
+N_SPLITS=5
+RANDOM_STATE=0
+FOLD_N=0
+
+# WD
 WEIGHT_DECAY=0
+
+# BATCH_SIZES
 BATCH_SIZE_GRU_SUBJ=4096
 BATCH_SIZE_GRU_POL=512
 BATCH_SIZE_TRANSFORMER_SUBJ=64
 BATCH_SIZE_TRANSFORMER_POL=16
-EPOCHS=50
-EPOCHS_TRANSFORMER=5
-DEVICE="cuda" if torch.cuda.is_available() else "cpu"
-SAVE=True if DEVICE == "cuda" else False
-LR=10e-3
+
+# EPOCHS
+EPOCHS_GRU=50
+EPOCHS_TRANSFORMER=10
+
+# LEARNING RATES
+LR_GRU=10e-3
 LR_TRANSFORMER=5e-5
+
+# GRU SETTINGS
 EMBEDDING_SIZE=256
 GRU_SIZE=128
 PAD_TOKEN=0
 ATTENTION=False
-FILTER=True
+
+# MISC
+DEVICE="cuda" if torch.cuda.is_available() else "cpu"
+SAVE=True if DEVICE == "cuda" else False
+FILTER=True # Whether to filter or not objective sentences
