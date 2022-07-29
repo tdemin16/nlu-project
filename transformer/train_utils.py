a3dc6c09d36e6f0b65ab82bb5372709beb66c17d
Thomas <thomasdemin16@gmail.com>
2022-07-29 11:01:07 +0200

    Test new metrics

--- transformer/train_utils.py
+++ transformer/train_utils.py
@@ -39,14 +39,14 @@ def train(model, optimizer, train_dl):
 
 
 @torch.no_grad()
-def evaluate(model, val_dl):
+def evaluate(model, test_dl):
     cum_loss = 0.
-    cum_acc = 0.
-    cum_f1 = 0.
+    cum_y_est = []
+    cum_y = []
     loss_fn = nn.BCEWithLogitsLoss()
 
     model.eval()
-    for x, a, y in val_dl:
+    for x, a, y in test_dl:
         x = x.to(DEVICE)
         a = a.to(DEVICE)
         y = y.type(torch.float).to(DEVICE)
@@ -54,10 +54,11 @@ def evaluate(model, val_dl):
         y_est = model(x, attention_mask=a).logits
 
         loss = loss_fn(y_est, y.unsqueeze(-1))
-
         cum_loss += loss.item()
-        acc, f1 = metrics(torch.sigmoid(y_est), y)
-        cum_acc += acc
-        cum_f1 += f1
 
-    return cum_loss / len(val_dl), cum_acc / len(val_dl), cum_f1 / len(val_dl)
+        cum_y_est.extend(torch.round(torch.sigmoid(y_est.squeeze(-1))).cpu().detach().numpy())
+        cum_y.extend(y.cpu().detach().numpy())
+    
+    acc, f1 = metrics(cum_y_est, cum_y)
+
+    return cum_loss / len(test_dl), acc, f1
